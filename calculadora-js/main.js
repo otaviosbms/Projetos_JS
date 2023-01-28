f0150f12c1d0b6d06c37095bcbb88a0312dd4335
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-01-27 23:59:28 -0300

    Add files via upload

---
+++ calculadora-js/main.js
@@ -0,0 +1,23 @@
+var visor = document.getElementById("visor");
+
+var botao1 = document.getElementById('botao1');
+var botao2 = document.getElementById('botao2');
+var botao3 = document.getElementById('botao3');
+var botao4 = document.getElementById('botao4');
+var botao5 = document.getElementById('botao5');
+var botao6 = document.getElementById('botao6');
+var botao7 = document.getElementById('botao7');
+var botao8 = document.getElementById('botao8');
+var botao9 = document.getElementById('botao9');
+var botao0 = document.getElementById('botao0');
+var botaoponto = document.getElementById('botao.');
+
+var botaomais = document.getElementById('botao+');
+var botaomenos = document.getElementById('botao-');
+var botaomulti = document.getElementById('botaox');
+var botaodiv = document.getElementById('botao/');
+var botaoc = document.getElementById('botaoc');
+var botaoigual = document.getElementById('botaoigual');
+
+var conta;
+
